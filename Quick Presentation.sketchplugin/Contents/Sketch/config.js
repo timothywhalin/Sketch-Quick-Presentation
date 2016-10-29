7763a125986decca33bedb764390393444e5c00c
Bram Perry <bram@dearcomputer.nl>
2016-10-29 15:50:16 -0400

    Improve config comments

--- Quick Presentation.sketchplugin/Contents/Sketch/config.js
+++ Quick Presentation.sketchplugin/Contents/Sketch/config.js
@@ -1,11 +1,11 @@
 var Config = {
-    docSize : 1,                // Specify 1 for 1x, 2 for 2x, etc. for what size you're designing at.
-    margin : 20,                // Sets the margin around your presentation.
+    docSize : 1,                        // Specify 1 for 1x, 2 for 2x, etc. for what size you're designing at.
+    margin : 20,                        // Sets the margin in pixels around your presentation.
     artboardColor : '#E6E6E6',
     artboardTitle : 'example',
-    //titleAboveScreens : 'ScreenTitle',
-    fontType : 'Helvetica',     // Font type for titles above artboards.
-    fontColor : '#2F5060',      // Font color for titles above artboards.
-    fontSize : 18,              // Base font size for titles. This will double when docSize set to 2.
-    createArtboardShadows : true,       // Draw a shape with shadow behind each artboard
+    //titleAboveScreens : 'ScreenTitle',// Use this text above all artboards
+    fontType : 'Helvetica',             // Font type for titles above artboards.
+    fontColor : '#2F5060',              // Font color for titles above artboards.
+    fontSize : 18,                      // Base font size in pixels for titles. This will double when docSize set to 2.
+    createArtboardShadows : true,       // Draw a shape with shadow behind each artboard (true or false)
 };
